88e0720e5d31048ccd9741733aeb9aa07469f37c
larpinglarps <rnghydra33@gmail.com>
2025-08-04 09:55:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,28 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits, Partials, Events, ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
 
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages],
-  partials: [Partials.Channel], // needed to receive DMs
-});
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages], partials: ['CHANNEL'] });
 
-client.once(Events.ClientReady, () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-client.on(Events.InteractionCreate, async interaction => {
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return;
+
   try {
-    if (interaction.isChatInputCommand()) {
-      if (interaction.commandName === 'menu') {
-        const menu = new StringSelectMenuBuilder()
-          .setCustomId('select_menu')
-          .setPlaceholder('Select an option')
-          .addOptions([
-            {
-              label: 'Option 1',
-              description: 'This is option 1',
-              value: 'option_1',
-            },
-            {
-              label: 'Option 2',
-              description: 'This is option 2',
-              value: 'option_2',
-            },
-            {
-              label: 'Option 3',
-              description: 'This is option 3',
-              value: 'option_3',
-            },
-          ]);
-        
-        const row = new ActionRowBuilder().addComponents(menu);
-        await interaction.reply({ content: 'Please select an option:', components: [row], ephemeral: true });
-      }
-    } else if (interaction.isStringSelectMenu()) {
-      if (interaction.customId === 'select_menu') {
-        const selectedValue = interaction.values[0];
-        await interaction.update({ content: `You selected: **${selectedValue}**`, components: [] });
-      }
-    }
+    await command.execute(interaction);
   } catch (error) {
     console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({ content: 'There was an error while processing this interaction.', ephemeral: true });
-    } else {
-      await interaction.reply({ content: 'There was an error while processing this interaction.', ephemeral: true });
-    }
+    await interaction.reply({ content: 'There was an error executing this command!', ephemeral: true });
   }
 });
 
